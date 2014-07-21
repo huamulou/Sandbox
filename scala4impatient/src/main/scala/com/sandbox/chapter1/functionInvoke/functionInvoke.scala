733404a1cc20db43fc87df0de67842383dff9f79
qianjie <qianjie@admaster.com.cn>
2014-07-21 18:31:58 +0800

    upload new code

--- scala4impatient/src/main/scala/com/sandbox/chapter1/functionInvoke/functionInvoke.scala
+++ scala4impatient/src/main/scala/com/sandbox/chapter1/functionInvoke/functionInvoke.scala
@@ -10,8 +10,14 @@ object functionInvoke {
     val rvSqrt = sqrt(2)
     val rvPow = pow(2,4)
     val rvMin = min(2,4)
+    val num = BigInt.probablePrime(100,scala.util.Random)
+    val str = "Hello".distinct
+    val patchedStr = "Harry".patch(1,"ung",2)
     println(rvSqrt)  //1.4142135623730951
     println(rvPow)   //16.0
     println(rvMin)   //2
+    println(num)     //659429356655432188646173242293
+    println(str)     //Helo
+    println(patchedStr)   //Hungry
   }
 }

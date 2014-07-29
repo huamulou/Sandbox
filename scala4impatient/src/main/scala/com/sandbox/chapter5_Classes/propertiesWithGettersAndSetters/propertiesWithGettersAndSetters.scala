ee6c1c3d91f26732bc5089a7cafec9832dc128b7
= <=>
2014-07-30 00:52:34 +0800

    upload new files

---
+++ scala4impatient/src/main/scala/com/sandbox/chapter5_Classes/propertiesWithGettersAndSetters/propertiesWithGettersAndSetters.scala
@@ -0,0 +1,10 @@
+package com.sandbox.chapter5_Classes.propertiesWithGettersAndSetters
+
+/**
+ * Created by Jolin&Vash on 2014/7/30.
+ */
+object propertiesWithGettersAndSetters {
+  def main(args: Array[String]) {
+    
+  }
+}

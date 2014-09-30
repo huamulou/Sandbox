6fd4b9bea0d849bafc5fa5f80a2b25934bc0cb8e
qianjie <qianjie@admaster.com.cn>
2014-09-30 15:43:14 +0800

    upload new files

---
+++ scala4impatient/src/main/scala/com/sandbox/chapter18_AdvancedTypes/familyPolymorphism/familyPolymorphism.scala
@@ -0,0 +1,10 @@
+package com.sandbox.chapter18_AdvancedTypes.familyPolymorphism
+
+/**
+ * Created by jin1 on 2014/9/30.
+ */
+object familyPolymorphism {
+  def main(args: Array[String]) {
+     
+  }
+}
